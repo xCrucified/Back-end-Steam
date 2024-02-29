2f239f4288145191ccd0dcd5902c42b7340a928c
Klxmnvqq <igrovoi.almaz@gmail.com>
2024-02-29 03:52:26 +0200

    updated

--- business_logic/Errors.Designer.cs
+++ business_logic/Errors.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BusinessLogic {
+namespace business_logic {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BusinessLogic {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BusinessLogic.Errors", typeof(Errors).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("business_logic.Errors", typeof(Errors).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
